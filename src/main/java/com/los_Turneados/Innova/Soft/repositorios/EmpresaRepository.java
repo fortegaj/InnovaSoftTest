0f53bfbac696fc15b4a7279339127b925538211f
edwinri0s <edwinrios490@gmail.com>
2022-08-28 10:30:11 -0500

    Se crean los servicios de consultar y actualizar de la clase "Empresa".

---
+++ src/main/java/com/los_Turneados/Innova/Soft/repositorios/EmpresaRepository.java
@@ -0,0 +1,8 @@
+package com.los_Turneados.Innova.Soft.repositorios;
+
+import com.los_Turneados.Innova.Soft.modelos.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
+
+}
