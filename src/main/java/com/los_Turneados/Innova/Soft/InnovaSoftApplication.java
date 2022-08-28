b107c8203872c0839fb95c46af02409d0fa3bc8f
edwinri0s <edwinrios490@gmail.com>
2022-08-28 09:47:24 -0500

    Se crea un getMappin para retornar un mensaje

--- src/main/java/com/los_Turneados/Innova/Soft/InnovaSoftApplication.java
+++ src/main/java/com/los_Turneados/Innova/Soft/InnovaSoftApplication.java
@@ -2,9 +2,18 @@ package com.los_Turneados.Innova.Soft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class InnovaSoftApplication {
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hola mundo"
+
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(InnovaSoftApplication.class, args);
