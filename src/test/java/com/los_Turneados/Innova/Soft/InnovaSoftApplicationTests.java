0e6572f94ea4d6c50a84ef65895921b18e153d64
David1585 <100976020+David1585@users.noreply.github.com>
2022-08-28 09:12:46 -0500

    Se crea el archivo innova Soft

---
+++ src/test/java/com/los_Turneados/Innova/Soft/InnovaSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.los_Turneados.Innova.Soft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovaSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
