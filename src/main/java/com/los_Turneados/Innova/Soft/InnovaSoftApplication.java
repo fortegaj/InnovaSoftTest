7fa882aa62ddc3634d03f3b98bceba5c84c2bfb8
David1585 <100976020+David1585@users.noreply.github.com>
2022-08-28 09:12:46 -0500

    Se crea el archivo innova Soft

---
+++ src/main/java/com/los_Turneados/Innova/Soft/InnovaSoftApplication.java
@@ -0,0 +1,13 @@
+package com.los_Turneados.Innova.Soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovaSoftApplication.class, args);
+	}
+
+}
