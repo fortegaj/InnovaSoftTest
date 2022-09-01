aa14dba532a8e1b7696e15916603a58b14b1399d
fortegaj <f.r.o.j22@hotmail.com>
2022-08-31 21:48:32 -0400

    Se crean atributos y metodos constructor, getter y setter de la clase Empresa en la rama development

--- src/main/java/com/los_Turneados/Innova/Soft/modelos/Empleado.java
+++ src/main/java/com/los_Turneados/Innova/Soft/modelos/Empleado.java
@@ -1,3 +1,50 @@
 package com.los_Turneados.Innova.Soft.modelos;
 public class Empleado {
+
+    private String nombreEmpleado;
+    private String correoEmpleado;
+    private Empresa empresa;
+    private String rolEmpleado;
+
+    public Empleado(String nombreEmpleado, String correoEmpleado, Empresa empresa, String rolEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+        this.correoEmpleado = correoEmpleado;
+        this.empresa = empresa;
+        this.rolEmpleado = rolEmpleado;
+    }
+
+    public Empleado() {
+    }
+
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getCorreoEmpleado() {
+        return correoEmpleado;
+    }
+
+    public void setCorreoEmpleado(String correoEmpleado) {
+        this.correoEmpleado = correoEmpleado;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getRolEmpleado() {
+        return rolEmpleado;
+    }
+
+    public void setRolEmpleado(String rolEmpleado) {
+        this.rolEmpleado = rolEmpleado;
+    }
 }
