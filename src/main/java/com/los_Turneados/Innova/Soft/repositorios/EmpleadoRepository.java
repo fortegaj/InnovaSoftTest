30ebed9180d72f241c127381ba7331454e45dc34
fortegaj <f.r.o.j22@hotmail.com>
2022-09-02 06:34:47 -0400

    Se crea la interfaz EmpleadoRepository en el package repositorios

---
+++ src/main/java/com/los_Turneados/Innova/Soft/repositorios/EmpleadoRepository.java
@@ -0,0 +1,7 @@
+package com.los_Turneados.Innova.Soft.repositorios;
+
+import com.los_Turneados.Innova.Soft.modelos.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
+}
