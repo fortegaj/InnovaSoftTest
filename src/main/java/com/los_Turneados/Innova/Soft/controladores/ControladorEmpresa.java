5e190ebe6662f708e7e71bf08bb4d938c20562f2
edwinri0s <edwinrios490@gmail.com>
2022-09-03 23:01:48 -0500

    Se crean los arichivos HTML de empresa y se actualizan las dependencias en el pom

--- src/main/java/com/los_Turneados/Innova/Soft/controladores/ControladorEmpresa.java
+++ src/main/java/com/los_Turneados/Innova/Soft/controladores/ControladorEmpresa.java
@@ -3,6 +3,7 @@ package com.los_Turneados.Innova.Soft.controladores;
 import com.los_Turneados.Innova.Soft.modelos.Empresa;
 import com.los_Turneados.Innova.Soft.servicios.EmpresaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,12 +12,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
-
+@Controller
 public class ControladorEmpresa {
     @Autowired
     EmpresaService empresaService;
 
-    @GetMapping({"/","/VerEmpresas"})
+    @GetMapping ({"/","/VerEmpresas"})
     public String viewEmpresas(Model model){
         List<Empresa> listaEmpresas=empresaService.getAllEmpresas();
         model.addAttribute("emplist",listaEmpresas);
@@ -74,3 +75,4 @@ public class ControladorEmpresa {
     }
 
 }
+
